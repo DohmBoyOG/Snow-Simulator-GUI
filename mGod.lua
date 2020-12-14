fcacfe7a644380a13bc59821a9b874d239877460
Tek9x <sean.schramm@live.com>
2020-12-14 10:59:20 -0500

    Update mGod.lua

--- mGod.lua
+++ mGod.lua
@@ -1,7 +1,7 @@
-for _, v in pairs(game:GetService("Players").DohmBoyOG.PlayerScripts.ThisGame:GetChildren()) do 
+for _, v in pairs(game:GetService("Players").LocalPlayer.PlayerScripts.ThisGame:GetChildren()) do 
     if v:IsA('ModuleScript') and v.Name == 'snowballProjectileClient' then 
         for _, s in pairs(getgc()) do 
-            if type(s) == "function" and getfenv(s).script == game:GetService("Players").DohmBoyOG.PlayerScripts.ThisGame[v.Name] then 
+            if type(s) == "function" and getfenv(s).script == game:GetService("Players").LocalPlayer.PlayerScripts.ThisGame[v.Name] then 
                 if debug.getinfo(s).name == 'minionSnowball' then 
                     hookfunction(s, function(...)
                         print('Nice, try minions cant hurt us!')
